054c6b5bc9a3016cb24b193c842f2466314dffce
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-09-01 11:22:27 -0700

    Added search by planning number.

--- application/views/scans/locations.php
+++ application/views/scans/locations.php
@@ -22,7 +22,7 @@
 
 			<div class="card px-4 py-8 flex justify-center items-center text-center lg:transform hover:scale-110 hover:shadow-lg transition-transform duration-200">
 				<a href="<?php echo base_url() ?>scans/new/<?php echo $location['location_id'] ?>">
-					<div>
+					<div data-content="<?php echo $location['planner_id'] ?>">
 						<span class="text-primary text-5xl leading-none la la-id-card"></span>
 						<p class="mt-2"><?php echo $location['location_name'] ?></p>
 						<div class="text-primary mt-5 text-3xl leading-none"><?php echo $location['planner_id'] ?></div>
@@ -35,5 +35,35 @@
 	</div>
 </div>
 
+<script
+		src="https://code.jquery.com/jquery-3.6.0.min.js"
+		integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+		crossorigin="anonymous"></script>
+
+<script>
+	function filterTextInput()
+	{
+		var input, radios, radio_filter, text_filter, td0, i, divList;
+		input = document.getElementById("myInput");
+		text_filter = input.value.toUpperCase();
+		divList = $(".div");
+
+		// Loop through all table rows, and hide those who don't match the search query
+		for (i = 0; i < divList.length; i++)
+		{
+			td0 = divList[i].getAttribute('data-content');
+			if (td0)
+			{
+				if (td0.toUpperCase().indexOf(text_filter) > -1) {
+					divList[i].style.display = "";
+				}
+				else
+				{
+					divList[i].style.display = "none";
+				}
+			}
+		}
+	}
+</script>
 
 
