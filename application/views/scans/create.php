3e2de0b161fb1e2da28ed4db6f51f2083a449c34
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-09-08 19:25:52 -0700

    Removing cdn and adding local js files.

--- application/views/scans/create.php
+++ application/views/scans/create.php
@@ -72,8 +72,10 @@
 
 
 
-
+<!--
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+-->
+<script src="<?php echo base_url() ?>assets/datatables/jquery.js"></script>
 
 
 <script>
--- application/views/scans/locations.php
+++ application/views/scans/locations.php
@@ -36,11 +36,13 @@
 
 	</div>
 </div>
-
+<!--
 <script
 		src="https://code.jquery.com/jquery-3.6.0.min.js"
 		integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
 		crossorigin="anonymous"></script>
+-->
+<script src="<?php echo base_url() ?>assets/datatables/jquery.js"></script>
 
 <script>
 	function filterTextInput()
