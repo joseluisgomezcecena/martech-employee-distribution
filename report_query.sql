a2d1c549f3b25e2c0c7cfa5855ac8b2f3d8bb1e0
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-09-21 11:51:41 -0700

    Added 2 more SQL Queries.

--- report_query.sql
+++ report_query.sql
@@ -1,3 +1,8 @@
 SELECT scans.emp_number, scans.location, locations.planner_id, scans.hours_worked, SUM(scans.hours_worked) AS suma
 FROM `scans`
 LEFT JOIN locations ON locations.location_id = scans.location WHERE created_at BETWEEN '2022-09-12' AND '2022-09-18' GROUP BY emp_number,location order by emp_number ASC
+
+
+SELECT emp_number, MAX(best) FROM (SELECT emp_number, SUM(hours_worked) as best FROM scans WHERE created_at BETWEEN '2022-09-12' AND '2022-09-18' GROUP BY emp_number, location) as bests GROUP BY emp_number
+
+SELECT emp_number, MAX(best),planner_id FROM (SELECT emp_number, SUM(hours_worked) as best,planner_id FROM scans LEFT JOIN locations ON locations.location_id = scans.location WHERE created_at BETWEEN '2022-09-12' AND '2022-09-18' GROUP BY emp_number, location) as bests GROUP BY emp_number
